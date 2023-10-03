44732af2b7eed504f3b17c35b6c6000099cfb442
Ravi <raviswami899@gmail.com>
2023-10-03 22:28:03 +0530

    about_page

--- src/container/landingp/landing.js
+++ src/container/landingp/landing.js
@@ -35,7 +35,7 @@ const Home = () => {
         
       </div>
       <div className="right-section">
-        {/* <img src="https://o.remove.bg/downloads/c883d2e4-4246-454f-ab0e-94cf63ad4b4a/dbt1auf-bded01c3-5d5f-46d3-aa59-7a8918057a02-removebg-preview.png" alt="Image 1" /> */}
+        
         <img src={image} alt="Image 2" />
       </div>
     </div>
