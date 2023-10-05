4a0666483012ba0a1b3e42874b30578f7ab3d6de
Ravi <raviswami899@gmail.com>
2023-10-05 20:05:15 +0530

    update landing page

--- src/container/landingp/landing.js
+++ src/container/landingp/landing.js
@@ -1,41 +1,81 @@
+// import React from 'react';
+// import './landing.css';
+// import image from './img1.png'
+// import {BiLogoTelegram} from 'react-icons/bi'
+
+// const Home = () => {
+//   return (
+//     <div className="container">
+//       <div className="left-section">
+//       <div className="icon-container" style={{}}>
+          
+          
+//           <div style={{color:"azure"}}><BiLogoTelegram/></div>
+//           <div style={{color:"azure"}}><BiLogoTelegram/></div>
+//           <div style={{color:"azure"}}><BiLogoTelegram/></div>
+//           <div style={{color:"azure"}}><BiLogoTelegram/></div>
+//           <div style={{color:"azure"}}><BiLogoTelegram/></div>
+//           <div style={{color:"azure"}}><BiLogoTelegram/></div>
+          
+
+            
+//         </div>
+//         <div className="text" style={{margin:"2rem"}}>
+//           <h2 >Take a part in the world biggest Crypto hunt.</h2>
+//           <div style={{fontSize:"15px",margin:"0", left:"0"}}>Discover a geolocation based play to earn mobile game combining social and game element where you can win free tokens</div>
+//           <div className="buttons">
+//           <button style={{justifyContent:"center",marginTop:"2rem",background:" rgb(86, 12, 235",color:"azure"}}>WHITELIST FOR EARLY ACCESS</button>
+          
+//         </div>
+//         <div className="bottom-button" style={{display:"flex"}}>
+//           <button style={{background:"#31065f",color:"azure"}}>Download App</button>
+//           <button style={{marginLeft:"10px",background:"#31065f",color:"azure"}}>Watch Video</button>
+//         </div>
+//         </div>
+        
+//       </div>
+//       <div className="right-section">
+        
+//         <img src={image} alt="Image 2" />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Home;
+
 import React from 'react';
 import './landing.css';
-import image from './img1.png'
-import {BiLogoTelegram} from 'react-icons/bi'
+import image from './img1.png';
+import { BiLogoTelegram } from 'react-icons/bi';
 
 const Home = () => {
   return (
     <div className="container">
       <div className="left-section">
-      <div className="icon-container" style={{}}>
-          
-          
-          <div><BiLogoTelegram/></div>
-          <div><BiLogoTelegram/></div>
-          <div><BiLogoTelegram/></div>
-          <div><BiLogoTelegram/></div>
-          <div><BiLogoTelegram/></div>
-          <div><BiLogoTelegram/></div>
-          
-
-            
+        <div className="icon-container">
+          <div style={{color:"azure"}}><BiLogoTelegram /></div>
+          <div style={{color:"azure"}}><BiLogoTelegram /></div>
+          <div style={{color:"azure"}}><BiLogoTelegram /></div>
+          <div style={{color:"azure"}}><BiLogoTelegram /></div>
+          <div style={{color:"azure"}}><BiLogoTelegram /></div>
+          <div style={{color:"azure"}}><BiLogoTelegram /></div>
         </div>
-        <div className="text" style={{margin:"2rem"}}>
-          <h2 >Take a part in the world biggest Crypto hunt.</h2>
-          <div style={{fontSize:"15px",margin:"0", left:"0"}}>Discover a geolocation based play to earn mobile game combining social and game element where you can win free tokens</div>
+        <div className="text">
+          <h2>Take a part in the world's biggest Crypto hunt.</h2>
+          <div className="description">
+            Discover a geolocation-based play-to-earn mobile game combining social and game elements where you can win free tokens.
+          </div>
           <div className="buttons">
-          <button style={{justifyContent:"center",marginTop:"2rem",background:" rgb(86, 12, 235",color:"azure"}}>WHITELIST FOR EARLY ACCESS</button>
-          
+            <button style={{background:"blue", color:"azure",fontSize:"1.5rem",marginTop:"2rem"}}>WHITELIST FOR EARLY ACCESS</button>
+          </div>
+          <div className="bottom-button">
+            <button style={{background:"#31065f", color:"azure",fontSize:"1.5rem"}}>Download App</button>
+            <button style={{marginLeft:"1rem", background:"#31065f", color:"azure",fontSize:"1.5rem"}}>Watch Video</button>
+          </div>
         </div>
-        <div className="bottom-button" style={{display:"flex"}}>
-          <button style={{background:"#31065f",color:"azure"}}>Download App</button>
-          <button style={{marginLeft:"10px",background:"#31065f",color:"azure"}}>Watch Video</button>
-        </div>
-        </div>
-        
       </div>
       <div className="right-section">
-        
         <img src={image} alt="Image 2" />
       </div>
     </div>
